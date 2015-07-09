5cf6f5ff19725e162fba8262ab7e203599eb0439
Vanova <iva427@mail.ru>
2015-07-09 13:24:07 +0300

    fixed checking

--- run_attrib.sh
+++ run_attrib.sh
@@ -19,8 +19,8 @@ compute-fbank-feats --verbose=2 --config=$fbank_config scp:$scp ark,scp,t:$fark,
 trans=model/fbank_to_splice.trans
 nnet=model/rbm_dbn_2_1024.nnet
 feats="ark:copy-feats scp:$fscp ark:- | add-deltas --delta-order=2 ark:- ark:- |"
-res=res/scores1.txt
-nnet-forward --no-softmax --feature-transform=$trans $nnet "$feats" ark,t:- > $res
+res=res/scores.txt
+nnet-forward --feature-transform=$trans $nnet "$feats" ark,t:- > $res
 
 [ ! -f $res ] && echo "[error] something went wrong..." && exit 1;
-echo "$0: attributes scores successfully extracted: $res";
+echo "[info] attributes scores successfully extracted: $res";
